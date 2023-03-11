6b2d4ebde6447ac6689dce2eb324bdd69bd78801
Yuvraj Singh <yuvi7860808034@gmail.com>
2023-03-11 12:29:26 +0530

    hero disc text made responsive

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,7 +8,9 @@ const Hero = () => (
     <div className={`flex-1 ${style.flexStart} flex-col xl:px-0 sm:px-16 px-6`}>
       <div className="flex flex-row items-center py-[4px] px-4 bg-discount-gradient rounded-[10px] mb-2">
         <img src={discount} alt="discount" className="w-[32px] h-[32px]" />
-        <p className={`${style.paragraph} ml-2 flex gap-1 flex-wrap text-sm md:text-base`}>
+        <p
+          className={`${style.paragraph} ml-2 flex gap-1 flex-wrap text-xs md:text-base`}
+        >
           <span className="text-white">20%</span>
           <span className="break-words">Discount For</span>{" "}
           <span className="text-white">1 Month</span> Account
