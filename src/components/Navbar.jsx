fc05172923a218cc02a791948fb37214b9437d02
Yuvraj Singh <yuvi7860808034@gmail.com>
2023-03-11 17:14:14 +0530

    stats

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,9 @@ const Navbar = () => {
   const [toggle, setToggle] = useState(false);
   return (
     <nav className="w-full flex py-6 justify-between items-center navbar">
-      <img src={logo} alt="logo" className="w-[124px] h-[32px]" />
+      <a href="/">
+        <img src={logo} alt="logo" className="w-[124px] h-[32px]" />
+      </a>
       <ul className="list-none sm:flex hidden justify-end items-center flex-1 gap-16">
         {navLinks.map((nav, index) => (
           <li
